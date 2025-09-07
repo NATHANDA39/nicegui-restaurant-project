14a8b18fef94b40477bf7da048ce38883148b5f7
Nathan Lartey <nathanlartey39@yahoo.com>
2025-09-07 23:32:02 +0000

    Welcome Page

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ app.add_static_files("/assets", "assets")
 
 ui.add_head_html('''<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />''')
 
+# Load google font
+ui.add_head_html('''
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <style>
+        .font-montserrat { font-family: "Montserrat", sans-serif; }
+    </style>
+''')
+
+
 ui.add_head_html('<link rel="stylesheet" href="/assets/reset.css"/>')
 
 hero.render()
-# welcome.render()
+welcome.render()
 
 ui.run()
--- sections/welcome.py
+++ sections/welcome.py
@@ -1,6 +1,17 @@
-from nicegui import ui 
+from nicegui import ui
 
-def render ():
-    ui.label("welcome message goes here")
+def render():
+    with ui.element().classes('flex justify-between w-screen h-screen p-0 mt-0').style('background-color: #efefef'):
+        
+        with ui.element("div").classes('flex flex-col justify-center items-center w-1/2 h-full p-10'):
+           ui.label('Italian Restaurant').classes('text-2xl font-bold italic font-montserrat')
+           ui.label('WELCOME').classes('text-6xl font-semibold mt-2 font-montserrat')
+           ui.html('Donec quis lorem nulla. Nunc eu hate me. Morbi nec lobortis est. Sed <br>fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi, ac facilesis <br>ligula sem id neque.') \
+                .classes('mt-4 text-center font-montserrat leading-relaxed')
+           ui.link("OUR STORY").classes("no-underline mt-6 text-gray-800 hover:text-red-600 transition-colors duration-300 font-montserrat")
+        
+        with ui.element("div").classes("flex justify-center items-center w-1/2 h-full"):
+            ui.image('assets/welcome.jpg').classes("rounded-lg w-[400px] h-[400px] object-cover p-4 ml-10 transition-transform duration-300 hover:scale-110")
+
 
 ui.run()
